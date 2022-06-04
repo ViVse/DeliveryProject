29a3c1405f517501d33359c1b4f9f6b94ddd9d8f
ViVse <ivanisenko730@gmail.com>
2022-06-04 22:35:39 +0300

    change customerId type

--- DAL/Entities/Review.cs
+++ DAL/Entities/Review.cs
@@ -6,7 +6,7 @@
         public int Stars { get; set; }
         public DateTime Date { get; set; }
         public int ShopId { get; set; }
-        public int CustomerId { get; set; }
+        public string CustomerId { get; set; }
         public Shop Shop { get; set; }
         public User Customer { get; set; }
     }
