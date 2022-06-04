4aacf022a6df85870ad9eba788b3a14075980c7f
ViVse <ivanisenko730@gmail.com>
2022-06-04 23:15:00 +0300

    added configurations for DeliveryMan and Review

---
+++ DAL/Configurations/DeliveryManConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using DAL.Entities;
+using DAL.Seeding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configurations
+{
+    public class DeliveryManConfiguration : IEntityTypeConfiguration<DeliveryMan>
+    {
+        public void Configure(EntityTypeBuilder<DeliveryMan> builder)
+        {
+            builder.Property(d => d.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(d => d.FirstName)
+               .HasMaxLength(50)
+               .IsRequired();
+
+            builder.Property(d => d.LastName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(d => d.Phone)
+                .IsRequired()
+                .HasMaxLength(10)
+                .IsFixedLength(true);
+
+            new DeliveryManSeeder().Seed(builder);
+        }
+    }
+}
