08235bb1e02dc5026a4fb2838a66236a2570b343
ViVse <ivanisenko730@gmail.com>
2022-06-04 22:41:31 +0300

    added seeding for DeliveryMan and Review

---
+++ DAL/Seeding/DeliveryManSeeder.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Seeding
+{
+    public class DeliveryManSeeder : ISeeder<DeliveryMan>
+    {
+        private static readonly List<DeliveryMan> workers = new()
+        {
+            new DeliveryMan
+            {
+                Id = 1,
+                FirstName = "Alex",
+                LastName = "Bob",
+                Phone = "0990502341"
+            },
+            new DeliveryMan
+            {
+                Id = 2,
+                FirstName = "John",
+                LastName = "Fisher",
+                Phone = "0950507641"
+            }
+        };
+
+        public void Seed(EntityTypeBuilder<DeliveryMan> builder) => builder.HasData(workers);
+    }
+}
