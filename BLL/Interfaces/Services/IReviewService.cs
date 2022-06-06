2e600dbd81552130638b6cb55442e828c7c7fd5c
ViVse <ivanisenko730@gmail.com>
2022-06-06 14:17:11 +0300

    Added ReviewService

---
+++ BLL/Interfaces/Services/IReviewService.cs
@@ -0,0 +1,21 @@
+﻿using BLL.DTO.Requests;
+using BLL.DTO.Responses;
+
+namespace BLL.Interfaces.Services
+{
+    public interface IReviewService
+    {
+        Task<IEnumerable<ReviewResponse>> GetAsync();
+
+        //ToDo
+        //Task<PagedList<ReviewResponse>> GetAsync(ReviewParameters parameters);
+
+        Task<ReviewResponse> GetByIdAsync(int id);
+
+        Task InsertAsync(ReviewRequest request);
+
+        Task UpdateAsync(ReviewRequest request);
+
+        Task DeleteAsync(int id);
+    }
+}
---
+++ BLL/Services/ReviewService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using BLL.DTO.Requests;
+using BLL.DTO.Responses;
+using BLL.Interfaces.Services;
+using DAL.Entities;
+using DAL.Interfaces;
+
+namespace BLL.Services
+{
+    public class ReviewService: IReviewService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public ReviewService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await unitOfWork.ReviewRepository.DeleteAsync(id);
+            unitOfWork.Commit();
+        }
+
+        public async Task<IEnumerable<ReviewResponse>> GetAsync()
+        {
+            var reviews = await unitOfWork.ReviewRepository.GetAllAsync();
+            return reviews?.Select(mapper.Map<Review, ReviewResponse>);
+        }
+
+        public async Task<ReviewResponse> GetByIdAsync(int id)
+        {
+            var review = await unitOfWork.ReviewRepository.GetByIdAsync(id);
+            return mapper.Map<Review, ReviewResponse>(review);
+        }
+
+        public async Task InsertAsync(ReviewRequest request)
+        {
+            var review = mapper.Map<ReviewRequest, Review>(request);
+            await unitOfWork.ReviewRepository.InsertAsync(review);
+            unitOfWork.Commit();
+        }
+
+        public async Task UpdateAsync(ReviewRequest request)
+        {
+            var review = mapper.Map<ReviewRequest, Review>(request);
+            await unitOfWork.ReviewRepository.UpdateAsync(review);
+            unitOfWork.Commit();
+        }
+    }
+}
