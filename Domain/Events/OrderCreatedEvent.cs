c92d64e6760b554b9733a4ea15e55000f72b3b39
ViVse <ivanisenko730@gmail.com>
2022-06-07 22:57:56 +0300

    created order events

---
+++ Domain/Events/OrderCreatedEvent.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+using Domain.Entities;
+
+namespace Domain.Events
+{
+    public class OrderCreatedEvent: BaseEvent
+    {
+        public Order Order { get; set; }
+
+        public OrderCreatedEvent(Order order)
+        {
+            Order = order;
+        }
+    }
+}
---
+++ Domain/Events/OrderDeletedEvent.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+using Domain.Entities;
+
+namespace Domain.Events
+{
+    public class OrderDeletedEvent : BaseEvent
+    {
+        public Order Order { get; set; }
+
+        public OrderDeletedEvent(Order order)
+        {
+            Order = order;
+        }
+    }
+}
