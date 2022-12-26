93dc8349bedcfa8479e28df9aa59c87619a332c9
ViVse <ivanisenko730@gmail.com>
2022-12-26 22:08:39 +0200

    updated ocelot

--- ApiGateways/OcelotApiGw/Program.cs
+++ ApiGateways/OcelotApiGw/Program.cs
@@ -1,8 +1,41 @@
 using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var authentificationProviderKey = "TestKey";
+
+builder.Services.AddAuthentication()
+    .AddJwtBearer(authentificationProviderKey, x =>
+    {
+        x.TokenValidationParameters = new()
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JwtSecurityKey"])),
+            ClockSkew = TimeSpan.Zero,
+        };
+
+        x.Events = new JwtBearerEvents
+        {
+            OnAuthenticationFailed = context =>
+            {
+                Console.WriteLine(context.Exception.Message);
+                if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                {
+                    context.Response.Headers.Add("Token-Expired", "true");
+                }
+                return Task.CompletedTask;
+            }
+        };
+    });
 
 builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
 
